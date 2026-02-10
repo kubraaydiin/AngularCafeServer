d2dd60cf0a4db511ce54fce704ab6ef71ab86550
kubraaydiin <kubraaydiin96@gmail.com>
2026-02-10 12:05:24 +0300

    Add project files.

---
+++ AngularCafeServer/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using AngularCafeServer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularCafeServer.Context
+{
+    public class AppDbContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+    }
+}
---
+++ AngularCafeServer/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace AngularCafeServer.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public IList<Menu> Menus { get; set; }
+    }
+}
